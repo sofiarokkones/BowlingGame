6d223cca691a834288e9d7663edff7a4319e4b93
Sofia Rokkones <sofia.rokkones@forefront.se>
2021-05-10 07:36:40 +0200

    Init gamer and test projects

---
+++ BowlingGamer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BowlingGamer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
